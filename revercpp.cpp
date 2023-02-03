615b2ddf6c083f32c1b1d20f870a0e804f25ab41
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-02-03 17:30:51 +0900

    Add quit command

--- revercpp.cpp
+++ revercpp.cpp
@@ -1,7 +1,26 @@
 #include <ncurses.h>
 
-int main(void) {
+const int BOARD_SIZE = 8;
+
+int main(void)
+{
   initscr();
-  getch();
+  noecho();
+  curs_set(0);
+
+  mvprintw(0, 0, "Reversi Game!");
+
+  mvprintw(BOARD_SIZE, 0, "Press 'q' to quit.");
+
+  while (true)
+  {
+    int ch = getch();
+
+    if (ch == 'q')
+    {
+      break;
+    }
+  }
+
   endwin();
 }
