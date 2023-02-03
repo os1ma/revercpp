33287bb694fc704092515da86e756d3bad877359
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-02-03 17:50:41 +0900

    Add Boost

--- revercpp.cpp
+++ revercpp.cpp
@@ -1,19 +1,45 @@
 #include <ncurses.h>
+#include <boost/format.hpp>
 
 const int BOARD_SIZE = 8;
 
-int main(void)
+struct Point
+{
+  int x;
+  int y;
+} Point;
+
+void init()
 {
   initscr();
   noecho();
   curs_set(0);
+}
 
+void show(struct Point p)
+{
   mvprintw(0, 0, "Reversi Game!");
 
+  auto output = boost::format("%d %d") % p.x % p.y;
+  auto str = output.str().c_str();
+  mvprintw(1, 0, str);
+
   mvprintw(BOARD_SIZE, 0, "Press 'q' to quit.");
+}
+
+int main(void)
+{
+  init();
+
+  struct Point p
+  {
+    3, 4
+  };
 
   while (true)
   {
+    show(p);
+
     int ch = getch();
 
     if (ch == 'q')
